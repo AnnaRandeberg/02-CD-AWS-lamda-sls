ac609a504443d9ba29acdcc7ed52ae5cf69a2bda
AnnaRandeberg <anna-randeberg@hotmail.com>
2024-10-22 20:07:22 +0000

    Simple change: renamed sentiment key to detected_sentiment

--- sentiment-demo/lambda/app.py
+++ sentiment-demo/lambda/app.py
@@ -13,6 +13,6 @@ def handler(event, context):
                 "Content-Type": "application/json"
             },
             "body": json.dumps({
-                "sentiment ": json.dumps(sentiment)
+                "detected_sentiment ": json.dumps(sentiment)
             })
     }
